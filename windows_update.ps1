b4e838bb37157cb9836b43bdd17c0aeffe0fe836
Michal <Michal@MICHAL-PC>
2021-07-18 11:11:54 +0200

    Změna způsobu dočasné deaktivace Windows defenderu. Automatická samo-aktualizace aktualizačního scriptu.

--- windows_update.ps1
+++ windows_update.ps1
@@ -1,6 +1,23 @@
-#Stop Windows Defender
-Write-Host "#Windows Defender stop"
-./dControl.exe /D
+Write-Host "#Autoupdate..."
+$URL = "https://raw.githubusercontent.com/cesnekmichal/windows_update/master/windows_update.ps1"
+$item1 = Invoke-WebRequest -Uri $URL | select -ExpandProperty Content
+if($item1 -ne $null -and $item1 -ne ""){
+    $item2 = (Get-Content -Path .\windows_update.ps1)
+    if(Compare-Object -ReferenceObject $item1 -DifferenceObject $item2 ){
+        Write-Host "#New version detected..."
+        Set-Content -Path .\windows_update.ps1 -Value $item1
+        Powershell.exe -File .\windows_update.ps1
+        Exit
+    } else {
+        Write-Host "#None new version detected."
+    }
+} else {
+    Write-Host "#Autoupdate failed :-("
+}
+
+#Disable Windows Defender Realtime Monitoring.
+Write-Host "#Windows Defender Realtime Monitoring disabled."
+Set-MpPreference -DisableRealtimeMonitoring $true
 
 #Install module Nuget
 if (Get-Module -ListAvailable -Name 'Nuget') {
@@ -9,6 +26,7 @@ if (Get-Module -ListAvailable -Name 'Nuget') {
 else {
     Write-Host "#Nuget Install..."
     Find-PackageProvider -Name 'Nuget' -ForceBootstrap -IncludeDependencies
+    Install-Package 'NuGet' -Force -Confirm:$False
     Write-Host "#Nuget Installed"
 }
 
@@ -34,17 +52,9 @@ Install-WindowsUpdate -install -acceptall -autoreboot
 Write-Host "#Microsoft Store Updates..."
 Get-CimInstance -Namespace "Root\cimv2\mdm\dmmap" -ClassName "MDM_EnterpriseModernAppManagement_AppManagement01" | Invoke-CimMethod -MethodName UpdateScanMethod
 
-#Start Windows Defender
-Write-Host "#Windows Defender start"
-./dControl.exe /E
-
-#Waiting to start Windows Defender
-do{
-    Start-Sleep -s 3
-    Write-Host "#Waiting to start Windows Defender..."
-    ./dControl.exe /Q | echo
-    Start-Sleep -s 3
-} while ($LASTEXITCODE -ne 0)
+#Enable Windows Defender Realtime Monitoring.
+Write-Host "#Windows Defender Realtime Monitoring enabled."
+Set-MpPreference -DisableRealtimeMonitoring $false
 
 #Update Windows Defender
 Write-Host "#Windows Defender Updating..."
@@ -52,3 +62,11 @@ Update-MpSignature
 
 #DONE
 Write-Host "#DONE :-)"
+
+#FINAL SLEEP
+$i = 1
+do{
+  Start-Sleep -s 1
+  Write-Host -NoNewline "."
+  $i++
+} while ($i -le 5)
