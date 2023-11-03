161f74d5811b88c431d80b3ee52dd056595c87a7
Michal <Michal@MICHAL-PC>
2023-11-03 14:28:52 +0100

    Verze 3.0

--- windows_update_mklink.cmd
+++ windows_update_mklink.cmd
@@ -55,13 +55,13 @@ if "%1"=="SKIP_SELF_UPDATE" (
 cd /D "%~dp0"
 
 :: Copying cmd files to local windows_update location
-mkdir "%HOMEDRIVE%%HOMEPATH%\AppData\Local\windows_update"
-copy "%~dp0\windows_update.cmd"        "%HOMEDRIVE%%HOMEPATH%\AppData\Local\windows_update\windows_update.cmd" /Y
-copy "%~dp0\windows_update_mklink.cmd" "%HOMEDRIVE%%HOMEPATH%\AppData\Local\windows_update\windows_update_mklink.cmd" /Y
+mkdir "%USERPROFILE%\AppData\Local\windows_update"
+copy "%~dp0\windows_update.cmd"        "%USERPROFILE%\AppData\Local\windows_update\windows_update.cmd" /Y
+copy "%~dp0\windows_update_mklink.cmd" "%USERPROFILE%\AppData\Local\windows_update\windows_update_mklink.cmd" /Y
 
 :: Creating Shortcut
-set "fileLnk=%HOMEDRIVE%%HOMEPATH%\Desktop\Aktualizovat Windows.lnk"
-set "fileCmd=%HOMEDRIVE%%HOMEPATH%\AppData\Local\windows_update\windows_update.cmd"
+set "fileLnk=%USERPROFILE%\Desktop\Aktualizovat Windows.lnk"
+set "fileCmd=%USERPROFILE%\AppData\Local\windows_update\windows_update.cmd"
 PowerShell.exe -Command "$WshShell = New-Object -comObject WScript.Shell; $Shortcut = $WshShell.CreateShortcut('%fileLnk%'); $Shortcut.TargetPath = '%fileCmd%'; $Shortcut.Save();"
 
 :: Executing Shortcut
